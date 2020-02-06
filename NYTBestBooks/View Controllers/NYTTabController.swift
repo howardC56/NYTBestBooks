221855dc1d3800712121a098d8677bac4e729ec1
howardC56 <h2chang56@gmail.com>
2020-02-05 22:30:42 -0500

    common tabBarController

--- NYTBestBooks/View Controllers/NYTTabController.swift
+++ NYTBestBooks/View Controllers/NYTTabController.swift
@@ -7,16 +7,36 @@
 //
 
 import UIKit
+import DataPersistence
 
 class NYTTabController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-
+        viewControllers = [firstVC,secondVC,thirdVC]
     }
-    
 
+    let dataPersistence = DataPersistence<Book>(filename: "favoriteBooks.plist")
+    
+    private lazy var firstVC: UINavigationController = {
+    let navController = UINavigationController(rootViewController: NYTBestSellersController())
+   navController.tabBarItem = UITabBarItem(title: "NYT Best Sellers",
+                                            image: UIImage(systemName: "book"), selectedImage: UIImage(systemName: "book.fill"))
+   return navController
+ }()
+    
+    private lazy var secondVC: UINavigationController = {
+       let navController = UINavigationController(rootViewController: FavoritesController())
+      navController.tabBarItem = UITabBarItem(title: "Favorites",
+                                               image: UIImage(systemName: "star"), selectedImage: UIImage(systemName: "star.fill"))
+      return navController
+    }()
+    
+    private lazy var thirdVC: UIViewController = {
+       let viewController = SettingsController()
+      viewController.tabBarItem = UITabBarItem(title: "Settings",
+                                               image: UIImage(systemName: "gear"), selectedImage: UIImage(systemName: "gear"))
+      return viewController
+    }()
 
 }
--- NYTBestBooks/View Controllers/SettingsController.swift
+++ NYTBestBooks/View Controllers/SettingsController.swift
@@ -12,7 +12,7 @@ class SettingsController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .systemGreen
     }
     
 
