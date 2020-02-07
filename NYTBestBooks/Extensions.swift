7c49091a3efc0a4f359bb48c1a5c4201ef42cd90
howardC56 <h2chang56@gmail.com>
2020-02-07 01:00:16 -0500

    initial favorites view cell and extensions

---
+++ NYTBestBooks/Extensions.swift
@@ -0,0 +1,46 @@
+//
+//  Extensions.swift
+//  NYTBestBooks
+//
+//  Created by Howard Chang on 2/6/20.
+//  Copyright © 2020 Howard Chang. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIView {
+
+func anchor(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, paddingTop: CGFloat? = 0, paddingLeft: CGFloat? = 0, paddingBottom: CGFloat? = 0, paddingRight: CGFloat? = 0, width: CGFloat? = nil, height: CGFloat? = nil) {
+    
+    translatesAutoresizingMaskIntoConstraints = false
+    
+    if let top = top {
+        topAnchor.constraint(equalTo: top, constant: paddingTop ?? 0).isActive = true
+    }
+    
+    if let left = left {
+        leftAnchor.constraint(equalTo: left, constant: paddingLeft ?? 0).isActive = true
+    }
+    
+    if let bottom = bottom {
+        if let paddingBottom = paddingBottom {
+            bottomAnchor.constraint(equalTo: bottom, constant:  -paddingBottom).isActive = true
+        }
+    }
+    
+    if let right = right {
+        if let paddingRight = paddingRight {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+    }
+    
+    if let width = width {
+        widthAnchor.constraint(equalToConstant: width).isActive = true
+    }
+    
+    if let height = height {
+        heightAnchor.constraint(equalToConstant: height).isActive = true
+    }
+}
+}
--- NYTBestBooks/Models/FavoritesViewCell.swift
+++ NYTBestBooks/Models/FavoritesViewCell.swift
@@ -10,4 +10,102 @@ import UIKit
 
 class FavoritesViewCell: UICollectionViewCell {
     
+    private lazy var imageView: UIImageView = {
+        let image = UIImageView()
+        image.clipsToBounds = true
+        image.layer.cornerRadius = 10
+        return image
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14)
+        label.textAlignment = .center
+        label.adjustsFontSizeToFitWidth = true
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var rankText: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 15)
+        label.textAlignment = .center
+        label.adjustsFontSizeToFitWidth = true
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 18)
+        label.textAlignment = .center
+        label.adjustsFontSizeToFitWidth = true
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var moreOptionsButton: UIButton = {
+           let button = UIButton(frame: CGRect(x: 10, y: 10, width: 10, height: 10))
+             button.backgroundColor = .clear
+             button.layer.cornerRadius = 20
+             button.setBackgroundImage(UIImage(systemName: "ellipsis"), for: .normal)
+             button.tintColor = .black
+        return button
+    }()
+    
+    override init(frame: CGRect) {
+           super.init(frame:frame)
+           commonSetup()
+       }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureCell(_ book: Book) {
+        
+    }
+    
+    func commonSetup() {
+        layer.cornerRadius = 10
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowRadius = 5
+        layer.shadowOffset = .zero
+        layer.shadowOpacity = 0.5
+        layer.shadowPath = UIBezierPath(rect: bounds).cgPath
+//        layer.shouldRasterize = true
+//        layer.rasterizationScale = UIScreen.main.scale
+        backgroundColor = .white
+        imageViewConstraints()
+        deleteButtonConstraints()
+        rankLabelConstraints()
+        titleLabelConstraints()
+        descriptionLabelConstraints()
+    }
+    
+    private func imageViewConstraints() {
+           addSubview(imageView)
+        imageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 10, paddingLeft: 10, width: 124, height: 70)
+       }
+    
+    private func deleteButtonConstraints() {
+           moreOptionsButton.anchor(top: topAnchor, right: rightAnchor, paddingTop: 10, paddingRight: 10, width: 20, height: 20)
+       }
+    
+    private func rankLabelConstraints() {
+        addSubview(rankText)
+        rankText.anchor(top: moreOptionsButton.topAnchor, left: imageView.rightAnchor, right: rightAnchor, paddingTop: 14, paddingLeft: 12, paddingRight: 10, height: 16)
+    }
+    
+    private func titleLabelConstraints() {
+        addSubview(titleLabel)
+        titleLabel.anchor(top: rankText.bottomAnchor, left: imageView.rightAnchor, right: rightAnchor, paddingTop: 10, paddingLeft: 12, paddingRight: 10, height: 20)
+    }
+    
+    private func descriptionLabelConstraints() {
+        addSubview(descriptionLabel)
+        descriptionLabel.anchor(top: imageView.bottomAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 10, paddingLeft: 10, paddingBottom: 10, paddingRight: 10)
+    }
+    
+    
 }
