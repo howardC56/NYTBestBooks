5d1c79d73847135cce68fcff42809669274292d2
Chelsi Christmas <c.christmas95@gmail.com>
2020-02-07 15:39:44 -0500

    Set up constraints for book image view

--- NYTBestBooks/Views/BookDetailView.swift
+++ NYTBestBooks/Views/BookDetailView.swift
@@ -10,12 +10,40 @@ import UIKit
 
 class BookDetailView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    public lazy var bookImage: UIImageView = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    public lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        return label
+    } ()
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    private func commonInit() {
+        
+    }
+    
+    private func setupImageView() {
+        addSubview(bookImage)
+        bookImage.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            bookImage.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20),
+            bookImage.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            //TODO: Use multiplier to set height and width so that the size of the book changes with the size of the device.
+            bookImage.heightAnchor.constraint(equalTo: safeAreaLayoutGuide.heightAnchor, multiplier: 0.40),
+            bookImage.widthAnchor.constraint(equalTo: safeAreaLayoutGuide.widthAnchor, multiplier: 0.40)
+        ])
+    }
 
 }
