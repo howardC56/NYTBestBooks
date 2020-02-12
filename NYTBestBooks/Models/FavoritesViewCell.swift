eb53a8be2ada7d00b65ab5fa47bc7ffc5d1ac924
howardC56 <h2chang56@gmail.com>
2020-02-11 20:22:47 -0500

    added emptyView,favoriteView,Controller with dataPersistence

--- NYTBestBooks/Models/FavoritesViewCell.swift
+++ NYTBestBooks/Models/FavoritesViewCell.swift
@@ -8,16 +8,24 @@
 
 import UIKit
 
+protocol FavoritesViewCellDelegate: class {
+    func didPressMoreOptionsButton(cell: FavoritesViewCell, book: Book)
+}
+
 final class FavoritesViewCell: UICollectionViewCell {
     
-    private lazy var imageView: UIImageView = {
+    weak var delegate: FavoritesViewCellDelegate?
+    private var currentBook: Book!
+    
+    public lazy var imageView: UIImageView = {
         let image = UIImageView()
         image.clipsToBounds = true
         image.layer.cornerRadius = 10
+        image.image = UIImage(systemName: "photo")
         return image
     }()
     
-    private lazy var descriptionLabel: UILabel = {
+    public lazy var descriptionLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 14)
         label.textAlignment = .center
@@ -25,82 +33,94 @@ final class FavoritesViewCell: UICollectionViewCell {
         label.numberOfLines = 0
         label.layer.borderWidth = 0.5
         label.layer.borderColor = UIColor.black.cgColor
+        label.text = "SimsSImsSimsSimsSImsSims SimsSImsSims SimsSImsSims SimsSImsSims SimsSImsSims SimsSImsSims SimsSImsSims SimsSImsSims SimsSImsSims SimsSImsSims SimsSImsSims SimsSImsSimsSimsSImsSimsSimsSImsSimsSimsSImsSims"
         return label
     }()
     
-    private lazy var genreRankText: UILabel = {
+    public lazy var genreRankText: UILabel = {
         let label = UILabel()
         label.font = .boldSystemFont(ofSize: 15)
         label.adjustsFontSizeToFitWidth = true
         label.numberOfLines = 0
+        label.text = " Genre: Fiction , Rank: 2 "
         return label
     }()
     
-    private lazy var titleLabel: UILabel = {
+    public lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.font = .boldSystemFont(ofSize: 18)
         label.adjustsFontSizeToFitWidth = true
         label.numberOfLines = 0
+        label.text = "Test"
         return label
     }()
     
-    private lazy var moreOptionsButton: UIButton = {
-           let button = UIButton(frame: CGRect(x: 10, y: 10, width: 10, height: 10))
-             button.backgroundColor = .clear
-             button.layer.cornerRadius = 20
-             button.setBackgroundImage(UIImage(systemName: "ellipsis"), for: .normal)
-             button.tintColor = .black
-             button.animateButtonView(button)
+    public lazy var moreOptionsButton: UIButton = {
+        let button = UIButton(frame: CGRect(x: 10, y: 10, width: 10, height: 10))
+        button.backgroundColor = .clear
+        button.layer.cornerRadius = 20
+        button.setBackgroundImage(UIImage(systemName: "ellipsis"), for: .normal)
+        button.tintColor = .black
+        button.addTarget(self, action: #selector(moreButtonPressed), for: .touchUpInside)
+        button.animateButtonView(button)
         return button
     }()
     
+    @objc func moreButtonPressed(_ sender: UIButton) {
+        delegate?.didPressMoreOptionsButton(cell: self, book: currentBook)
+    }
+    
     override init(frame: CGRect) {
-           super.init(frame:frame)
-           commonSetup()
-       }
+        super.init(frame:frame)
+        commonSetup()
+    }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
     func configureCell(_ book: Book) {
-        
+        currentBook = book
+        titleLabel.text = book.title
+        genreRankText.text = " Genre: , Rank: \(book.rank) "
+        descriptionLabel.text = book.description
     }
     
-   private func commonSetup() {
+    private func commonSetup() {
+        imageViewConstraints()
+        deleteButtonConstraints()
+        rankLabelConstraints()
+        titleLabelConstraints()
+        descriptionLabelConstraints()
         layer.cornerRadius = 10
         layer.shadowColor = UIColor.black.cgColor
         layer.shadowRadius = 5
         layer.shadowOffset = .zero
         layer.shadowOpacity = 0.5
         layer.shadowPath = UIBezierPath(rect: bounds).cgPath
-//        layer.shouldRasterize = true
-//        layer.rasterizationScale = UIScreen.main.scale
+        //        layer.shouldRasterize = true
+        //        layer.rasterizationScale = UIScreen.main.scale
         backgroundColor = .white
-        imageViewConstraints()
-        deleteButtonConstraints()
-        rankLabelConstraints()
-        titleLabelConstraints()
-        descriptionLabelConstraints()
     }
     
     private func imageViewConstraints() {
-           addSubview(imageView)
-        imageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 10, paddingLeft: 10, width: 124, height: 70)
-       }
+        addSubview(imageView)
+        imageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 10, paddingLeft: 10, width: 150, height: 250)
+    }
     
     private func deleteButtonConstraints() {
-           moreOptionsButton.anchor(top: topAnchor, right: rightAnchor, paddingTop: 10, paddingRight: 10, width: 20, height: 20)
-       }
+        addSubview(moreOptionsButton)
+        moreOptionsButton.anchor(top: imageView.topAnchor, right: rightAnchor, paddingRight: 10, width: 20, height: 20)
+    }
     
     private func rankLabelConstraints() {
         addSubview(genreRankText)
-        genreRankText.anchor(top: moreOptionsButton.topAnchor, left: imageView.rightAnchor, right: rightAnchor, paddingTop: 14, paddingLeft: 12, paddingRight: 10, height: 16)
+        genreRankText.anchor(top: moreOptionsButton.topAnchor, left: imageView.rightAnchor, right: rightAnchor, paddingTop: 30, paddingLeft: 12, paddingRight: 10, height: 16)
     }
     
     private func titleLabelConstraints() {
         addSubview(titleLabel)
-        titleLabel.anchor(top: genreRankText.bottomAnchor, left: imageView.rightAnchor, right: rightAnchor, paddingTop: 10, paddingLeft: 12, paddingRight: 10, height: 20)
+        titleLabel.anchor(top: genreRankText.bottomAnchor, left: imageView.rightAnchor, right: rightAnchor, paddingTop: 25, paddingLeft: 12, paddingRight: 10, height: 20)
     }
     
     private func descriptionLabelConstraints() {
--- NYTBestBooks/ViewControllers/FavoritesController.swift
+++ NYTBestBooks/ViewControllers/FavoritesController.swift
@@ -9,12 +9,97 @@
 import UIKit
 
 class FavoritesController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)
-
+    
+    private let favoriteView = FavoritesView()
+    //public var dataPersistence: DataPersistence<Book>!
+    
+    private var favoriteBooks = [Book]() {
+        didSet {
+            favoriteView.collectionview.reloadData()
+//             if favoriteBooks.isEmpty {
+//                favoriteView.collectionview.backgroundView = EmptyFavoritesView()
+//                       } else {
+//                           favoriteView.collectionview.backgroundView = nil
+//                       }
+        }
     }
     
+    override func loadView() {
+        if favoriteBooks.isEmpty {
+        view = EmptyFavoritesView()
+        title = "Empty Gallery - Add Some Books"
+        } else {
+        view = favoriteView
+        title = "Favorites"
+    }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        getFavoriteBooks()
+        favoriteView.collectionview.delegate = self
+        favoriteView.collectionview.dataSource = self
+        favoriteView.collectionview.register(FavoritesViewCell.self, forCellWithReuseIdentifier: "FavoritesViewCell")
+    }
+    
+    private func getFavoriteBooks() {
+        do {
+           //favoriteBooks = try dataPersistence.loadItems()
+        } catch {
+            print("cant fetch yo")
+        }
+    }
+    
+}
 
+extension FavoritesController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        //favoriteBooks.count
+       return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FavoritesViewCell", for: indexPath) as? FavoritesViewCell else { fatalError() }
+        cell.backgroundColor = .systemBackground
+        //let saved = favoriteBooks[indexPath.row]
+        //cell.configureCell(saved)
+        cell.delegate = self
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let detailVC = BookDetailController()
+        //detailVC.article = favoriteBooks[indexPath.row]
+        //detailVC.dataPersistence = dataPersistence
+        navigationController?.pushViewController(detailVC, animated: true)
+    }
+    
+}
+
+extension FavoritesController: FavoritesViewCellDelegate {
+    func didPressMoreOptionsButton(cell: FavoritesViewCell, book: Book) {
+        let alertController = UIAlertController(title: "Choose", message: "Book's Destiny", preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let delete = UIAlertAction(title: "Throw Out", style: .destructive) { [weak self] (alertAction) in
+            self?.deleteBook(book)
+        }
+        alertController.addAction(cancelAction)
+        alertController.addAction(delete)
+        if let popoverPresentationController = alertController.popoverPresentationController {
+               popoverPresentationController.sourceView = self.view
+               popoverPresentationController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0)
+                   popoverPresentationController.permittedArrowDirections = []
+               }
+        present(alertController,animated: true)
+    }
+    
+    private func deleteBook(_ book: Book) {
+        guard let index = favoriteBooks.firstIndex(of: book) else { return }
+        do {
+            //try dataPersistence.deleteItem(at: index)
+        } catch {
+            print("error deleting article: \(error)")
+        }
+    }
 }
